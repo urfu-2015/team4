7e98f5c83bce62a730ea55d4a857e8f8d37e7d6c
Anna1004 <anna.khlutkova@mail.ru>
2016-05-03 15:24:13 +0500

    fix

--- middleware/cookieAuthenticator.js
+++ middleware/cookieAuthenticator.js
@@ -1,15 +1,12 @@
 'use strict';
 
-const debug = require('debug')('team4:middleware:cookieAuthenticator');
-const salt = require('config').get("hash").cookieSalt;
-
 const hash = require('../lib/hash.js');
+const salt = require('config').get("hash").cookieSalt;
+const debug = require('debug')('team4:middleware:cookieAuthenticator');
 
 module.exports = () => {
     return (req, res, next) => {
         debug('check cookie');
-<<<<<<< HEAD
-=======
         if (req.commonData) {
             req.commonData.user = {};
         } else {
@@ -20,13 +17,13 @@ module.exports = () => {
         } else {
             res.commonData = {user: {}};
         }
->>>>>>> 6798274dc80ee943779fbaefb6e1e8d6be407cad
         var userId = req.cookies.id;
         if (userId) {
             var isLoggedIn = hash.validate(userId, salt);
             var userName = userId.split('.')[0];
             if (isLoggedIn) {
-                req.commonData.user = userName;
+                req.commonData.user.name = userName;
+                res.commonData.user.name = userName;
             }
         }
         next();

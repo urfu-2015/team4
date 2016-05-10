bcd6861c5587505887e3d63a06fb9007efb27050
Наташа <n.e.dolgih@yandex.ru>
2016-05-10 18:57:30 +0500

    fix

--- blocks/quest/quest.js
+++
@@ -1,1 +0,0 @@
-require('./quest.css');
--- controllers/pages.js
+++ controllers/pages.js
@@ -3,6 +3,7 @@
 const debug = require('debug')('team4:controllers:pages');
 
 const questsModel = require('../models/quests');
+const userModel = require('../models/users');
 const randInt = require('../lib/random').randInt;
 
 function filterFields(fields) {
@@ -41,12 +42,26 @@ exports.index = (req, res) => {
 exports.userPage = (req, res) => {
     debug('userPage');
     let users = userModel(req.db);
-    users.isNameExist(req.params.name)
+    users.isUserExist(req.params.name)
         .then(() => { // no such name
-            res.renderLayout('./pages/userPage/userPage.hbs', {error: 'no such user'})
+            var chosenQuests = [
+                {title:'Harold 1', photo:'http://i.imgur.com/LbDUJDk.jpg',url:'/'},
+                {title:'Harold 2', photo:'http://www.netlore.ru/upload/files/68338/large_p19d7nh1hm1i37tnuim11ebqo5c1.jpg',url:'/'},
+                {title:'Harold 3', photo:'http://i.imgur.com/WE8DG5F.jpg',url:'/'},
+                {title:'Harold 4', photo:'http://cs631327.vk.me/v631327103/19a66/VNzIvlvv2Ss.jpg',url:'/'},
+                {title:'Harold 5', photo:'http://ci.memecdn.com/108/5904108.jpg',url:'/'}
+            ];
+
+            res.renderLayout('./pages/userPage/userPage.hbs', 
+                {
+                    username: req.params.name,
+                    commonData: req.commonData,
+                    finished: chosenQuests
+                });
         })
         .catch(() => { //name exists
-            res.renderLayout('./pages/userPage/userPage.hbs', {username: req.params.name});
+            res.renderLayout('./pages/userPage/userPage.hbs',
+                {error: 'no such user', commonData: req.commonData})
         })
 };
 

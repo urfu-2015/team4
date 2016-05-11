830a3f6d5b9e57e6ed3972751b1a9e7e17d33ac3
Наташа <n.e.dolgih@yandex.ru>
2016-05-11 12:28:43 +0500

    feat(./userPage/*): страничка пользователя;  fix(): маленькие стилистические правки

--- controllers/pages.js
+++ controllers/pages.js
@@ -42,27 +42,46 @@ exports.index = (req, res) => {
 exports.userPage = (req, res) => {
     debug('userPage');
     let users = userModel(req.db);
+    var response = {
+        username: req.params.name,
+        commonData: req.commonData
+    };
     users.isUserExist(req.params.name)
-        .then(() => { // no such name
-            var chosenQuests = [
+        .then((users) => {
+            if (users > 0) {
+                return req.params.name;
+            } else {
+                res.renderLayout('./pages/notFound/notFound.hbs',
+                    {commonData: req.commonData});
+                throw Error('no user');
+            }
+        })
+        .then(users.getFinishedQuests)
+        .then((finished) => {
+            if (finished.length !== 0) {
+                Object.assign(response, {finished:finished})
+            }
+            return req.params.name;
+        })
+        .then(users.getQuestsInProgress)
+        .then((inProcess) => {
+            inProcess = [
                 {title:'Harold 1', photo:'http://i.imgur.com/LbDUJDk.jpg',url:'/'},
                 {title:'Harold 2', photo:'http://www.netlore.ru/upload/files/68338/large_p19d7nh1hm1i37tnuim11ebqo5c1.jpg',url:'/'},
                 {title:'Harold 3', photo:'http://i.imgur.com/WE8DG5F.jpg',url:'/'},
                 {title:'Harold 4', photo:'http://cs631327.vk.me/v631327103/19a66/VNzIvlvv2Ss.jpg',url:'/'},
                 {title:'Harold 5', photo:'http://ci.memecdn.com/108/5904108.jpg',url:'/'}
             ];
-
-            res.renderLayout('./pages/userPage/userPage.hbs', 
-                {
-                    username: req.params.name,
-                    commonData: req.commonData,
-                    finished: chosenQuests
-                });
-        })
-        .catch(() => { //name exists
-            res.renderLayout('./pages/userPage/userPage.hbs',
-                {error: 'no such user', commonData: req.commonData})
-        })
+            if (inProcess.length !== 0) {
+                Object.assign(response, {inProcess:inProcess});
+            }
+            console.log(req.params.name, response.commonData.user)
+            if (req.params.name == response.commonData.user) {
+                Object.assign(response, {self: true});
+            }
+            console.log(response);
+            res.renderLayout('./pages/userPage/userPage.hbs', response);
+        });
 };
 
 

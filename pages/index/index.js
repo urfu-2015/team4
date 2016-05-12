dde654289b998786c662522a3b5ffbdf68ad3681
Наташа <n.e.dolgih@yandex.ru>
2016-05-12 18:24:15 +0500

    fix(./controllers/pages, ./pages/index/index.js): Подгрузка квестов на главной

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,30 +8,31 @@ var skip = 0;
 $('#getMore').click(function (e) {
     e.preventDefault();
     $.ajax({
-        method: "POST",
-        url: "/getMoreQuests",
-        skip: skip + 10
-    })
-    .done(function (data) {
-        data.quests.forEach(function (quest) {
-            var newElem = $('<div></div>', {
-                class: 'col-lg-12 text-center'
+            method: "POST",
+            url: "/get-more-quests",
+            data: {skip: skip + 10}
+        })
+        .done(function (data) {
+            skip += 10;
+            data.quests.forEach(function (quest) {
+                var newElem = $('<div></div>', {
+                    class: 'col-lg-12 text-center'
+                });
+                $('<img>', {
+                    class: 'img-responsive img-border img-full',
+                    src: quest.photo,
+                    alt: quest.title
+                }).appendTo(newElem);
+                $('<h2></h2>', {
+                    text: quest.title
+                }).appendTo(newElem);
+                $('<a></a>', {
+                    href: '/quest/' + quest.url,
+                    class: 'btn btn-default btn-lg',
+                    text: 'Посмотреть'
+                }).appendTo(newElem);
+                newElem.append('<hr>');
+                $('#list-of-quests').append(newElem);
             });
-            $('<img>', {
-                class: 'img-responsive img-border img-full',
-                src: quest.photo,
-                alt: quest.title
-            }).appendTo(newElem);
-            $('<h2></h2>', {
-                text: quest.title
-            }).appendTo(newElem);
-            $('<a></a>', {
-                href: '/quest/' + quest.url,
-                class: 'btn btn-default btn-lg',
-                text: 'Посмотреть'
-            }).appendTo(newElem);
-            newElem.append('<hr>');
-            $('#list-of-quests').append(newElem);
         });
-    });
 });

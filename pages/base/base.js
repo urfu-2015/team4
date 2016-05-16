270b169fe2bf3271e1e35e4c0f0279dc3c93e466
Savi <Ultimate95@mail.ru>
2016-05-16 19:40:51 +0500

    fix(): document.onready()

--- pages/base/base.js
+++ pages/base/base.js
@@ -4,20 +4,21 @@ require('./base.css');
 require('../../blocks/header/header.js');
 require('../../blocks/footer/footer.js');
 
-$('#logout').click(function (e) {
-    e.preventDefault();
+$(function () {
+    $('#logout').click(function (e) {
+        e.preventDefault();
 
-    $.ajax({
-        method: 'POST',
-        url: '/user/logout'
-    })
+        $.ajax({
+            method: 'POST',
+            url: '/user/logout'
+        })
+        .done(function (msg) {
+            console.log(msg);
+            window.location = '/';
+        })
 
-    .done(function (msg) {
-        console.log(msg);
-        window.location = '/';
-    })
-
-    .fail(function (msg) {
-        console.log(msg);
+        .fail(function (msg) {
+            console.log(msg);
+        });
     });
 });

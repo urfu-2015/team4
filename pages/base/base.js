7eab15b75239d648f382eb1ddf7fdadfe5db38d3
Savi <Ultimate95@mail.ru>
2016-05-22 00:56:46 +0500

    fix/feat(): vk button + style fixes

--- pages/base/base.js
+++ pages/base/base.js
@@ -22,4 +22,8 @@ $(function () {
             console.error(msg);
         });
     });
+
+    $('#vk').attr('href', 'https://oauth.vk.com/authorize?' +
+        'client_id=5471140&display=page&redirect_uri=' + window.location.origin +
+        '/auth-vk&scope=email&response_type=code&v=5.52');
 });

b4affa6a301d45a86e29519defead92ff24d1294
PolinaKoval <polina.koval.32@gmail.com>
2016-05-21 17:46:11 +0500

    fix5

--- pages/authorization/authorization.js
+++ pages/authorization/authorization.js
@@ -37,10 +37,8 @@ $(function () {
             errorInfo.empty().append(msg.responseText).show();
         });
     });
-});
 
-window.onload = function () {
-    document.getElementById('vk').setAttribute('href', 'https://oauth.vk.com/authorize?' +
+    $('#vk').attr('href','https://oauth.vk.com/authorize?' +
         'client_id=5471140&display=page&redirect_uri=' + window.location.origin +
         '/auth-vk&scope=email&response_type=code&v=5.52');
-};
+});
